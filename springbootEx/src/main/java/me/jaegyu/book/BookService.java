d39271d98b483203d0ba25a0e1e7c51bf14798ae
jaeGyu <jpmlyr@gmail.com>
2016-03-08 17:53:26 +0900

    .

--- springbootEx/src/main/java/me/jaegyu/book/BookService.java
+++ springbootEx/src/main/java/me/jaegyu/book/BookService.java
@@ -6,6 +6,9 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Service;
 
 import me.jaegyu.book.BookDto.Create;
@@ -16,6 +19,9 @@ public class BookService {
 
 	@Autowired
 	private BookRepository repository;
+	
+	@Autowired
+	private PagingAndSortingRepository<Book, Long> pRepository;
 
 	public Book createBook(Create dto) {
 
@@ -32,5 +38,9 @@ public class BookService {
 	
 	public List<Book> getBooks(){
 		return repository.findAll();
+	}
+	
+	public Page<Book> getBooksToPage(Pageable pageable){
+		return pRepository.findAll(pageable);
 	}
 }
--- springbootEx/src/test/java/me/jaegyu/book/BookServiceTest.java
+++ springbootEx/src/test/java/me/jaegyu/book/BookServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -18,7 +20,7 @@ public class BookServiceTest {
 
 		BookDto.Create createDto = new BookDto.Create();
 		createDto.setName("Thinking in java");
-		createDto.setAuthor("bruku");
+		createDto.setAuthor("Bruce Eckel");
 		createDto.setPrice(43000l);
 
 		service.createBook(createDto);
@@ -30,6 +32,43 @@ public class BookServiceTest {
 		service.createBook(createDto);
 
 		System.out.println(service.getBooks());
+	}
+	
+	@Test
+	public void getBooksToPage(){
+		BookDto.Create createDto = new BookDto.Create();
+		createDto.setName("Thinking in java");
+		createDto.setAuthor("Bruce Eckel");
+		createDto.setPrice(43000l);
+		service.createBook(createDto);
+
+		createDto.clear();
+		createDto.setName("자바의 정석");
+		createDto.setAuthor("남궁성");
+		createDto.setPrice(25000l);
+		service.createBook(createDto);
+		
+		createDto.clear();
+		createDto.setName("자바의 정석2");
+		createDto.setAuthor("남궁성");
+		createDto.setPrice(25000l);
+		service.createBook(createDto);
+		
+		createDto.clear();
+		createDto.setName("자바의 정석3");
+		createDto.setAuthor("남궁성");
+		createDto.setPrice(25000l);
+		service.createBook(createDto);
+		
+		createDto.clear();
+		createDto.setName("Spring4");
+		createDto.setAuthor("최범균");
+		createDto.setPrice(28000l);
+		service.createBook(createDto);
+		
+		Page<Book> booksToPage = service.getBooksToPage(new PageRequest(1, 2));
+
+		System.out.println(booksToPage.getContent());
 	}
 
 }
