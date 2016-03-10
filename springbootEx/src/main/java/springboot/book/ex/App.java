ef9b8a6902d4437c6a8c5cc6cac8f7dc38d991f2
jaeGyu <jpmlyr@gmail.com>
2016-03-10 18:01:11 +0900

    .

--- springbootEx/src/main/java/springboot/book/ex/App.java
+++ springbootEx/src/main/java/springboot/book/ex/App.java
@@ -5,6 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @EnableAutoConfiguration
 @ComponentScan
@@ -15,11 +17,23 @@ public class App implements CommandLineRunner {
 
 	@Override
 	public void run(String... arg0) throws Exception {
+		Customer created3 = customerRepository.save(new Customer(null, "taekyong", "seo"));
+		Customer created2 = customerRepository.save(new Customer(null, "hojin", "kim"));
 		Customer created = customerRepository.save(new Customer(null, "Jaegyu", "Han"));
 
-		System.out.println(created + "is created!");
+		// System.out.println(created + "is created!");
 
 		customerRepository.findAll().forEach(System.out::println);
+
+		System.out.println("order by--------------------");
+
+		customerRepository.findAllOrderByName().forEach(System.out::println);
+
+		System.out.println("paging-----------------");
+
+		Pageable pageable = new PageRequest(1, 2);
+		customerRepository.findAllOrderByNameWithPaging(pageable).forEach(System.out::println);
+
 	}
 
 	public static void main(String[] args) {
--- springbootEx/src/main/java/springboot/book/ex/CustomerRepository.java
+++ springbootEx/src/main/java/springboot/book/ex/CustomerRepository.java
@@ -1,6 +1,17 @@
 package springboot.book.ex;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface CustomerRepository extends JpaRepository<Customer, Integer>{
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+	@Query("SELECT x From Customer x ORDER BY x.firstName, x.lastName")
+	List<Customer> findAllOrderByName();
+
+	@Query("SELECT x From Customer x ORDER BY x.firstName, x.lastName")
+	Page<Customer> findAllOrderByNameWithPaging(Pageable pageable);
 }
