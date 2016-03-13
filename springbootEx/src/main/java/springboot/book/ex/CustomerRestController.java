44d93910557bfd8d464c366c710d0a0366bff23a
jpmlyr <jpmlyr@gmail.com>
2016-03-13 23:46:50 +0900

    .

--- springbootEx/src/main/java/springboot/book/ex/CustomerRestController.java
+++ springbootEx/src/main/java/springboot/book/ex/CustomerRestController.java
@@ -1,15 +1,19 @@
 package springboot.book.ex;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("api/customers")
@@ -30,9 +34,17 @@ public class CustomerRestController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	@ResponseStatus(HttpStatus.CREATED)
-	public Customer postCustomers(@RequestBody Customer customer) {
-		return customerService.create(customer);
+//	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Object> postCustomers(@RequestBody Customer customer, UriComponentsBuilder uriBuilder) {
+		Customer created = customerService.create(customer);
+		
+		URI location = uriBuilder.path("api/customers/{id}")
+				.buildAndExpand(created.getId()).toUri();
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(location);
+		
+		return new ResponseEntity<>(); 
 	}
 
 	@RequestMapping(value = "{id}", method = RequestMethod.PUT)
