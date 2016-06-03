85e6744d7db9e1387494046f681380c7090ec073
jaeGyu <jpmlyr@gmail.com>
2016-06-03 14:10:48 +0900

    flyway 적용

---
+++ springbootEx/src/main/resources/db/migration/V1__create-schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE customers (id INT PRIMARY KEY AUTO_INCREMENT,
+                        first_name VARCHAR(30),
+                        last_name VARCHAR(30));
+                        
+CREATE TABLE posts (id INT PRIMARY KEY AUTO_INCREMENT,
+                    title VARCHAR(100),
+                    content VARCHAR(1000));
---
+++ springbootEx/src/main/resources/db/migration/V2__import-initial-data.sql
@@ -0,0 +1,6 @@
+insert into customers (first_name, last_name) values ('Nobita', 'Nobi');
+insert into customers (first_name, last_name) values ('Takeshi', 'Goda');
+insert into customers (first_name, last_name) values ('Suneo', 'Honekwa');
+insert into customers (first_name, last_name) values ('Shizuka', 'Minamoto');
+
+insert into posts (title, content) values ('test post','t post!!');
