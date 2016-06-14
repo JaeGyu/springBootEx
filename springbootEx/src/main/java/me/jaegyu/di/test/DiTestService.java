cd8b585c7deedb8605e0709c188b8f2529ba5a17
jaeGyu <jpmlyr@gmail.com>
2016-06-14 16:50:42 +0900

    코드 정리

--- springbootEx/src/main/java/me/jaegyu/di/test/DiTestService.java
+++ springbootEx/src/main/java/me/jaegyu/di/test/DiTestService.java
@@ -1,6 +1,5 @@
 package me.jaegyu.di.test;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
