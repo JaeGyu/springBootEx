037ff8cb49e7e9db585471d952d411f5ebf63f8a
jaeGyu <jpmlyr@gmail.com>
2016-08-19 14:42:11 +0900

    oAuth에서 accesstoken을 받기위해 서버로부터 code를 넘겨받아 code를 가지고 토큰 요청하기

---
+++ springbootEx/src/main/java/me/jaegyu/oauth2/AppForGetAccessToken.java
@@ -0,0 +1,12 @@
+package me.jaegyu.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppForGetAccessToken {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppForGetAccessToken.class, args);
+	}
+}
